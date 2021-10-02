f68862a9cdfce932968d96951461fdf8c43ba7d6
Natnicha24 <natnicha.ant@mail.kmutt.ac.th>
2021-10-02 11:21:57 +0700

    Midterm num3

---
+++ FlowerStore.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MT_Number3
+{
+    class FlowerStore
+    {
+        public List<string> flowerList = new List<string>();
+        List<string> cart = new List<string>();
+
+        public FlowerStore()
+        {
+            flowerList.Add("Rose");
+
+            flowerList.Add("Lotus");
+        }
+        public void addToCart(string name)
+        {
+            cart.Add(name);
+        }
+
+        public void showCart()
+        {
+            if (cart.Count == 0)
+            {
+                Console.WriteLine("Cart is empty");
+            }
+            else
+            {
+                Console.WriteLine("My Cart :");
+                foreach (string i in cart)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+        public void Printlistmenu(FlowerStore flowerStore)
+        {
+            Console.WriteLine("Select number for buy flower :");
+            foreach (string i in flowerStore.flowerList)
+            {
+                Console.Write((flowerStore.flowerList.IndexOf(i) + 1) + " ");
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MT_Number3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string decide = "y";
+            string selectFlower;
+            FlowerStore flowerStore = new FlowerStore();
+            do
+            {
+                flowerStore.Printlistmenu(flowerStore);
+                selectFlower = Console.ReadLine();
+
+                Addlistflower(flowerStore,selectFlower);
+                decide = InputdecideFromKeyBoard(flowerStore,decide);
+                Checkinputforexit(flowerStore, decide);
+            }
+            while (decide != "exit");
+        }
+
+        static string InputdecideFromKeyBoard(FlowerStore flowerStore,string decide)
+        {
+            Console.WriteLine("You can stop this progress ? exit for >> exit << progress and press any key for continue");
+            decide = Console.ReadLine();
+            return decide;
+        }
+
+        static void Addlistflower(FlowerStore flowerStore,string selectFlower)
+        {
+            switch (selectFlower)
+            {
+                case "1":
+                    flowerStore.addToCart(flowerStore.flowerList[0]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[0]);
+                    break;
+                case "2":
+                    flowerStore.addToCart(flowerStore.flowerList[1]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[1]);
+                    break;
+                default:
+                    Console.WriteLine("Not Added to cart. found select number of flower");
+                    break;
+            }
+        }
+        static void Checkinputforexit(FlowerStore flowerStore,string decide)
+        {
+            if (decide == "exit")
+            {
+                Console.Write("Current my cart");
+                flowerStore.showCart();
+            }
+        }
+    }
+}
+
